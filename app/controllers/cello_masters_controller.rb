2bdb8191ba2084dcd091f579a6c4a6d810234c90
Nikita shah <nikitashah606@gmail.com>
2019-12-15 17:20:03 +0530

    Upload csv and add data feature

--- app/controllers/cello_masters_controller.rb
+++ app/controllers/cello_masters_controller.rb
@@ -1,6 +1,7 @@
 class CelloMastersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_cello_master, only: [:show, :edit, :update, :destroy]
+  require 'csv'
 
   # GET /cello_masters
   # GET /cello_masters.json
@@ -52,6 +53,30 @@ class CelloMastersController < ApplicationController
     end
   end
 
+  def upload_csv
+    csv = File.read(params[:cello_master][:picture])
+    @cello_masters = []
+    CSV.parse(csv, headers: true).each do |row|
+      cello_master = CelloMaster.new
+      cello_master.company_name = row['company_name']
+      cello_master.divison = row['divison']
+      cello_master.category = row['category']
+      cello_master.product_name = row['product_name']
+      cello_master.capacity = row['capacity']
+      cello_master.volume = row['volume']
+      cello_master.mrp = row['mrp']
+      cello_master.drp = row['drp']
+      cello_master.trade_discount = row['trade_discount']
+      cello_master.rate = row['rate']
+      cello_master.scheme = row['scheme']
+      cello_master.net_rate = row['net_rate']
+      cello_master.product_image.attach(io: File.open(row['product_image']), filename: 'test.jpeg')
+      if cello_master.save
+        @cello_masters.push(cello_master)
+      end
+    end
+  end
+
   # DELETE /cello_masters/1
   # DELETE /cello_masters/1.json
   def destroy
@@ -70,6 +95,6 @@ class CelloMastersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def cello_master_params
-      params.require(:cello_master).permit(:company_name, :divison, :category, :product_name, :capacity, :volume, :mrp, :drp, :trade_discount, :rate, :scheme, :net_rate)
+      params.require(:cello_master).permit(:company_name, :divison, :category, :product_name, :capacity, :volume, :mrp, :drp, :trade_discount, :rate, :scheme, :net_rate, :product_image)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :cello_masters
   devise_for :users
+  post '/upload_csv', to: 'cello_masters#upload_csv', as: 'upload_csv'
   root to: 'cello_masters#index'
 end
