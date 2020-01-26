6cddf77c99da616532aa92c6d25b0b02e13dc179
Nikita shah <nikitashah606@gmail.com>
2020-01-26 18:31:48 +0530

    Solve default path host

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  only_path: true
+  default_url_options :host => "https://karnavati.herokuapp.com"
   resources :cello_masters
   devise_for :users
   post '/upload_csv', to: 'cello_masters#upload_csv', as: 'upload_csv'
